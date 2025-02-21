a6bf13063f0785d2d2bccfbee6cad201c7567e64
counquest2023 <dlqns928@naver.com>
2025-02-22 02:35:17 +0900

    FEAT-FeedList UI 전체 개선

--- src/test/java/es/board/SearchTests.java
+++ src/test/java/es/board/SearchTests.java
@@ -1,29 +1,29 @@
-package es.board;
-
-
-import es.board.repository.ScheduleDAO;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Component;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-public class SearchTests {
-
-    private final ScheduleDAO scheduleDAO;
-
-    @Autowired  // ✅ Spring이 자동 주입
-    public SearchTests(ScheduleDAO scheduleDAO) {
-        this.scheduleDAO = scheduleDAO;
-    }
-
-    @Test
-    void  searchCalender(){
-
-
-        System.out.println("scheduleDAO = " + scheduleDAO.searchSchedule("asd","title" ,"zzzz","soonest"));
-    }
-}
+//package es.board;
+//
+//
+//import es.board.repository.ScheduleDAO;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.stereotype.Component;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//public class SearchTests {
+//
+//    private final ScheduleDAO scheduleDAO;
+//
+////    @Autowired  // ✅ Spring이 자동 주입
+////    public SearchTests(ScheduleDAO scheduleDAO) {
+////        this.scheduleDAO = scheduleDAO;
+////    }
+////
+////    @Test
+////    void  searchCalender(){
+////
+////
+////        System.out.println("scheduleDAO = " + scheduleDAO.searchSchedule("asd","title" ,"zzzz","soonest"));
+////    }
+//}
