776521b7435a209a3b171cbbcd974aeac7031ad6
conquest2023 <dlqns928@naver.com>
2026-03-06 18:38:50 +0900

    FEAT-Pwa 구현

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -39,12 +39,12 @@ export default defineConfig({
         theme_color: '#82b1ff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
